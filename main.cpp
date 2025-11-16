39810c68ed7fd82a261679880b1cade8f5d2be11
nandanishrestha <nandanishrestha.98@gmail.com>
2025-11-16 14:30:50 +0545

    all file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "enter your name: ";
+    cin >> name;
+    cout << "your name is " << name << endl;
+    return 0;
+}
